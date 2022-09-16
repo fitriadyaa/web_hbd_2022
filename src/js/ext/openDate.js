2d2d8ad39aef4647dd520e6fd6957255371e6354
fitriadyaa <fitrianiwidyaf@gmail.com>
2022-09-17 00:29:36 +0700

    yen birthday

--- src/js/ext/openDate.js
+++ src/js/ext/openDate.js
@@ -1,6 +1,6 @@
 export const isBDay = function() {
     const startTime = new Date(process.env.OPEN_DATE + "T00:00").getTime();
-    const endTime = '2023-09-16';
+    const endTime = '2023-09-18';
     const localTime = Date.now();
     if (localTime < startTime) return "IS_EARLY";
     if (localTime > endTime) return "IS_LATE";
