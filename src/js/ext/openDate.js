30da6156475e7cd4287eb224ed756b81923c0fe3
fitriadyaa <fitrianiwidyaf@gmail.com>
2022-09-16 11:45:20 +0700

    last change

--- src/js/ext/openDate.js
+++ src/js/ext/openDate.js
@@ -1,8 +1,8 @@
-export const isBDay = function () {
-  const startTime = new Date(process.env.OPEN_DATE + "T00:00").getTime();
-  const endTime = startTime + 24 * 60 * 60 * 1000;
-  const localTime = Date.now();
-  if (localTime < startTime) return "IS_EARLY";
-  if (localTime > endTime) return "IS_LATE";
-  return "ON_TIME";
+export const isBDay = function() {
+    const startTime = new Date(process.env.OPEN_DATE + "T00:00").getTime();
+    const endTime = '2023-09-16';
+    const localTime = Date.now();
+    if (localTime < startTime) return "IS_EARLY";
+    if (localTime > endTime) return "IS_LATE";
+    return "ON_TIME";
 };
